41a7737b391a926770f37a04d64f73730b54ab32
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-13 08:26:56 -0700

    Update pages

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
+/**
+ * ------------------------------------------------------ /
+ * The main container for imports and navigation
+ * Default page is xxxx.js
+ * ------------------------------------------------------ /
+ */
+
+// Main Imports ----------------------------------------- /
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+// Auth Screens Import ---------------------------------- /
+import Home from './components/Home';
+import Lobby from './components/Lobby';
+import Login from './components/Login';
+import Splash from './components/Splash';
+
+
+// Navigation ------------------------------------------ /
+const Nav = createAppContainer(
+  createStackNavigator({
+    Home: { screen: Home },
+  })
+)
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Nav />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/Home.js
+++ components/Home.js
@@ -1,1 +1,51 @@
 // Home Page
+
+import React, { Component } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default class Home extends Component {
+  // Hide the header bar section
+  static navigationOptions = {
+    header: null
+  }
+
+  logging = () => {
+    console.log('Hi');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={require('../resources/logo.png')} />
+        <Text style={styles.h1}>Login to start partying</Text>
+        <TouchableOpacity onPress={this.logging}>
+          <Image style={styles.buttonFacebook} source={require('../resources/button-facebook.png')} />
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 200,
+    width: 200,
+  },
+  h1: {
+    color: '#B300B3',
+    fontSize: 20,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  buttonFacebook: {
+    height: 40,
+    width: 300,
+  },
+});
