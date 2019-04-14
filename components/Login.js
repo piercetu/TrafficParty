fcec9bc40b9219cba2043c32acae09cc97c6eaac
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-13 22:03:50 -0700

    Update Logic UI

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 // Login Page
 import React, { PureComponent } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 
 import { connect } from 'react-redux';
@@ -28,7 +28,7 @@ class Login extends PureComponent {
       });
     }, 3000);
   }
-  
+
   handleFacebookLoginOnPress = () => {
     this.props.facebookLogin();
   }
@@ -60,8 +60,25 @@ class Login extends PureComponent {
 
         <Image style={styles.logo} source={require('../resources/logo.png')} />
         <Text style={styles.h1}>Login to start partying</Text>
-        <TouchableOpacity onPress={this.handleFacebookLoginOnPress}>
-          <Image style={styles.buttonFacebook} source={require('../resources/button-facebook.png')} />
+
+        {/* Facebook Login Button */}
+        <TouchableOpacity style={styles.buttonBlue} onPress={this.logging}>
+          <Text style={styles.buttonText}>LOG IN WITH FACEBOOK</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.label}>Email</Text>
+        <TextInput style={styles.input} placeholder="e.g. john.smith@gmail.com" />
+        <Text style={styles.label}>Password</Text>
+        <TextInput style={styles.input} placeholder="e.g. Must be at least 6 characters" />
+
+        {/* Sign in Button */}
+        <TouchableOpacity style={styles.buttonPurple}>
+          <Text style={styles.buttonText}>SIGN IN</Text>
+        </TouchableOpacity>
+
+        {/* Create account Button */}
+        <TouchableOpacity style={styles.buttonTeal}>
+          <Text style={styles.buttonText}>CREATE AN ACCOUNT</Text>
         </TouchableOpacity>
       </View>
     );
@@ -76,24 +93,71 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logo: {
-    height: 200,
-    width: 200,
+    height: 120,
+    width: 120,
   },
   h1: {
     color: '#B300B3',
     fontSize: 20,
-    fontWeight: '500',
+    // fontWeight: '500',
+    marginTop: 15,
+    marginBottom: 20,
     textAlign: 'center',
   },
   buttonFacebook: {
     height: 40,
     width: 300,
   },
+  label: {
+    fontWeight: '500',
+    marginTop: 6,
+    marginBottom: 6,
+    textAlign: 'left',
+    width: '85%',
+  },
+  input: {
+    height: 45,
+    width: '85%',
+    textAlign: 'left',
+    borderWidth: 1,
+    borderColor: '#cfd0d1',
+    paddingLeft: 8,
+  },
+  buttonBlue: {
+    alignItems: 'center',
+    backgroundColor: '#3c5a99',
+    height: 45,
+    marginBottom: 20,
+    marginTop: 20,
+    justifyContent: 'center',
+    width: '85%',
+  },
+  buttonPurple: {
+    alignItems: 'center',
+    backgroundColor: '#C239B3',
+    height: 45,
+    marginBottom: 20,
+    marginTop: 20,
+    justifyContent: 'center',
+    width: '85%',
+  },
+  buttonTeal: {
+    alignItems: 'center',
+    backgroundColor: '#0d9aa2',
+    height: 45,
+    justifyContent: 'center',
+    width: '85%',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '500',
+  },
 });
+
 
 const mapStateToProps = state => {
   return {
-    user: state.auth.user 
+    user: state.auth.user
   };
 };
 
